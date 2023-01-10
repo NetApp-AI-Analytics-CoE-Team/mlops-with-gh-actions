81c9e2f30d4ba64467c2fdb42b1270d3ca6f4bff
yshimizu37 <yshimizu@netapp.com>
2023-01-10 12:19:30 +0000

    fix snapshotter

--- pipeline_components/netapp_snapshot.py
+++ pipeline_components/netapp_snapshot.py
@@ -5,38 +5,23 @@ def create_snapshot(
   volume_snapshot_class :str="csi-snapclass",
   ) -> str:
   import netapp_dataops.k8s as ndt
-  # import datetime
-
-  # Generate snapshot name with timestamp
-  # timestamp = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime('%Y%m%d%H%M%S')
-  # snapshot_name = pvc_name + "-" + timestamp
 
   # Take snapshot
+  print(f"Namespacce: {namespace}")
+  print(f"PVC name: {pvc_name}")
+  print(f"Snapshot name: {snapshot_name}")
+  print(f"VolumeSnapshotClass name: {volume_snapshot_class}")
+
   try: 
     ndt.create_volume_snapshot(
       pvc_name=pvc_name, 
       snapshot_name=snapshot_name, 
       volume_snapshot_class=volume_snapshot_class, 
       namespace=namespace)
-
-    # output = {
-    #   "pvc_name": pvc_name, 
-    #   "snapshot_name": snapshot_name, 
-    #   "volume_snapshot_class": volume_snapshot_class, 
-    #   "namespace": namespace,
-    #   "message": "Taking snapshot has be successfully completed"
-    # }
     print("Taking snapshot has be successfully completed")
+
   # Error handling
-  except InvalidConfigError:
-    # output = {
-    #     "message": "Failed taking snapshot because of invalid kubeconfig"
-    # }
-    print("Failed taking snapshot because of invalid kubeconfig")
-  except APIConnectionError:
-    # output = {
-    #     "message": "Failed taking snapshot because of connection to the Kubernetes API"
-    # }
-    print("Failed taking snapshot because of connection to the Kubernetes API")
+  except Exception as e:
+    print(e)
 
   return snapshot_name
