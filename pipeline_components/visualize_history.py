0a3187e4f09bd5d2d5bf5590054e382a155b5c9c
yshimizu37 <yshimizu@netapp.com>
2023-01-11 00:31:34 +0000

    add visualizer

--- pipeline_components/visualize_history.py
+++ pipeline_components/visualize_history.py
@@ -2,7 +2,8 @@ from kfp.components import InputPath,OutputPath
 
 def markdown_vis(
     input_history_path:InputPath('history'),
-    mlpipeline_ui_metadata_path: OutputPath()
+    mlpipeline_ui_metadata_path: OutputPath(),
+    mlpipeline_metrics_path: OutputPath('Metrics')
     ):
     import json
     import pandas
@@ -13,18 +14,44 @@ def markdown_vis(
 
     # convert history json to markdown
     df = pandas.DataFrame.from_dict(data)
-    df_md = df.to_markdown(index=False)
+    df.insert(0, 'epochs', range(1, len(df.index) + 1))
+    df_md = df.to_markdown(index=False, tablefmt="grid")
         
     metadata = {
         'outputs' : [
         # Markdown that is hardcoded inline
         {
             'storage': 'inline',
-            'source': f'# Training Result\n{df_md}',
+            'source': f'# Training Result\n{df_md}\n',
             'type': 'markdown',
         },
         ]
     }
 
+    # write markdown visualizer
     with open(mlpipeline_ui_metadata_path, 'w') as metadata_file:
         json.dump(metadata, metadata_file)
+
+    # get last epoch's metrics
+    accuracy = df.tail(1).iloc[0]["accuracy"]
+    val_accuracy = df.tail(1).iloc[0]["val_accuracy"]
+
+    # create metrics
+    metrics = {
+        'metrics': [
+            {
+            'name': 'accuracy-score', # The name of the metric. Visualized as the column name in the runs table.
+            'numberValue':  accuracy, # The value of the metric. Must be a numeric value.
+            'format': "PERCENTAGE",   # The optional format of the metric. Supported values are "RAW" (displayed in raw format) and "PERCENTAGE" (displayed in percentage format).
+            },
+            {
+            'name': 'validation-accuracy-score', # The name of the metric. Visualized as the column name in the runs table.
+            'numberValue':  val_accuracy, # The value of the metric. Must be a numeric value.
+            'format': "PERCENTAGE",   # The optional format of the metric. Supported values are "RAW" (displayed in raw format) and "PERCENTAGE" (displayed in percentage format).
+            },
+        ]
+    }
+
+    # write metrics data
+    with open(mlpipeline_metrics_path, 'w') as f:
+        json.dump(metrics, f)
