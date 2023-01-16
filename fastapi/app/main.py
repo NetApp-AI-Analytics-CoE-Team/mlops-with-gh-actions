f86719852c4a5f8822bf4c7e5cddeceb4de7e561
yshimizu37 <yshimizu@netapp.com>
2023-01-16 03:35:43 +0000

    fix cache option

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -6,6 +6,10 @@ import imghdr
 import PIL.Image
 import numpy as np
 import json
+import base64
+
+NAMESPACE = "production"
+MODEL_NAME = "mlops-demo"
 
 def get_internal_service_url(namespace:str, model_name:str):
     internal_service_url = f"http://{model_name}.{namespace}.svc.cluster.local/v1/models/{model_name}"
@@ -16,7 +20,8 @@ def is_image_binary(binary: bytes) -> bool:
     return image_type is not None
 
 class Image(BaseModel):
-    url: str
+    # url: str
+    data_b64: str 
 
 app = FastAPI()
 
@@ -24,36 +29,39 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/models/{namespace}/{model_name}")
-async def get_model_info(namespace:str, model_name:str):
-    url = get_internal_service_url(namespace=namespace, model_name=model_name)
+@app.get("/model")
+async def get_model_info():
+    url = get_internal_service_url(namespace=NAMESPACE, model_name=MODEL_NAME)
     response = requests.get(url)
     res_json = json.dumps(response.json(), indent=2)
     res_dict = json.loads(res_json)
     return res_dict
 
-@app.get("/models/{namespace}/{model_name}/metadata")
-async def get_model_info(namespace:str, model_name:str):
-    url = get_internal_service_url(namespace=namespace, model_name=model_name) + "/metadata"
+@app.get("/model/metadata")
+async def get_model_info():
+    url = get_internal_service_url(namespace=NAMESPACE, model_name=MODEL_NAME) + "/metadata"
     response = requests.get(url)
     res_json = json.dumps(response.json(), indent=2)
     res_dict = json.loads(res_json)
     return res_dict
 
-@app.post("/models/{namespace}/{model_name}")
-async def post_inference_request(namespace:str, model_name:str, images: List[Image]):
-    LABELS = ["daisy", "dandelion", "roses", "sunflower", "tulip"]
-    url = get_internal_service_url(namespace=namespace, model_name=model_name) + ":predict"
+@app.post("/classify")
+async def post_inference_request(images: List[Image]):
+    # LABELS = ["daisy", "dandelion", "roses", "sunflower", "tulip"]
+    LABELS = ['both', 'fumi', 'unknown', 'watage']
+    url = get_internal_service_url(namespace=NAMESPACE, model_name=MODEL_NAME) + ":predict"
     
     image_byte_list = []
     for image in images:
         # download image from requested url
-        urlData = requests.get(image.url).content
-        is_image_binary(urlData)
+        # urlData = requests.get(image.url).content
+        # is_image_binary(urlData)
+
+        data_binary = base64.b64decode(image.data_b64)
 
         IMAGE_PATH = "/tmp/image"
         with open(IMAGE_PATH ,mode='wb') as f: # wb でバイト型を書き込める
-            f.write(urlData)
+            f.write(data_binary)
 
         image = PIL.Image.open(IMAGE_PATH).convert("RGB").resize((299, 299))
         image = np.array(image) / 255
